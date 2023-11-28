668d4444fc605dc52a8b1b9f4726202fe433fa09
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-11-28 05:47:40 -0800

    this is the task's commit

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import random
+
+# Assign a random signed number to the variable 'number'
+number = random.randint(-10000, 10000)
+
+# Get the last digit of the number
+last_digit = abs(number) % 10
+
+# Print the result
+print("Last digit of", number, "is", last_digit, end=' ')
+
+# Check conditions for the last digit
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
+
