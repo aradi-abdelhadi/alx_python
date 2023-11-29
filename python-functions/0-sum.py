32e9f084bba57779d6fa15bd12827064ea07f017
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-11-29 03:28:39 -0800

    this is the task's commit

---
+++ python-functions/0-sum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+    
+    :param a: The first integer.
+    :param b: The second integer.
+    :return: The sum of a and b.
+    """
+    return a + b
+
+# Test cases
+print(add(1, 2))    # Output: 3
+print(add(98, 0))    # Output: 98
+print(add(100, -2))  # Output: 98
+
