a414cf176409fc7fa76e32237514785a6e074cba
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-11-30 03:08:44 -0800

    this is the task's commit

---
+++ python-functions/3-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+reverse_string = __import__('3-string').reverse_string
+
+# Test cases
+print(reverse_string("Hello"))          # Output: olleH
+print(reverse_string(""))                # Output: (empty string)
+print(reverse_string("madam"))           # Output: madam
+print(reverse_string("Hello, World!"))   # Output: !dlroW ,olleH
+
---
+++ python-functions/3-string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def reverse_string(string):
+    """
+    Reverses a given string.
+
+    :param string: The input string.
+    :return: The reversed string.
+    """
+    return string[::-1]
+
