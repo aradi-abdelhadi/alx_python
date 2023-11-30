6ed2cdbe881f420d4e1ce9515de096bba417d438
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-11-30 03:03:46 -0800

    this is the task's commit

---
+++ python-functions/0-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+add = __import__('0-sum').add
+
+# Test cases
+print(add(1, 2))    # Output: 3
+print(add(98, 0))    # Output: 98
+print(add(100, -2))  # Output: 98
+
--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
+
 def add(a, b):
     """
     Adds two integers and returns the result.
-
+    
     :param a: The first integer.
     :param b: The second integer.
     :return: The sum of a and b.
     """
     return a + b
-
-# Test cases
-print("Correct output - case: add(1, 2):", add(1, 2))        # Output: 3
-print("Correct output - case: add(100, -2):", add(100, -2))  # Output: 98
-print("Correct output - case: add(-100, -2):", add(-100, -2))  # Output: -102
-print("Correct output - case: add(0, 0):", add(0, 0))          # Output: 0
 
