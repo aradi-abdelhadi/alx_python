fc983e6df5621b9fa6ce457c6b1e2cf7d6e4eb98
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-03 08:25:12 -0800

    this is the task's commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     num_args = len(argv) - 1
     args_list = argv[1:]
 
-    print("{} {}{}{}".format(
+    print("{}{}{}".format(
         "{} argument{}".format(num_args, 's' if num_args != 1 else ''),
-        ':' if num_args > 0 else '.',
+        ' :' if num_args > 0 else '.',
         '\n' if num_args > 0 else '',
         '\n'.join(["{}: {}".format(i, arg) for i, arg in enumerate(args_list, 1)])
     ))
