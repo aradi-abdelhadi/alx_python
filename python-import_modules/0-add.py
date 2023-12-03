cd9afb0a323cd859235b4027d8fb41668b1421fa
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-03 08:13:19 -0800

    this is the task's commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     from add_0 import add as fake_add
 
     result = fake_add(a, b)
-    print("{} - {} => {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, result))
 
