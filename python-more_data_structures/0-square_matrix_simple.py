57fa89a60a697b53b3e48daddb78325968ada002
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-05 03:39:55 -0800

    this is the task's commit

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,13 +3,17 @@ def square_matrix_simple(matrix=[]):
     return [[x**2 for x in row] for row in matrix]
 
 if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
+    # Test cases
+    test_cases = [
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        [[1, 2], [4, 5]],
+        [[1, 2], [4, 5], [7, 8]],
+        [[1]],
+        [[1], [2], [3], [4]],
+        [[]]
     ]
 
-    new_matrix = square_matrix_simple(matrix)
-    print(new_matrix)
-    print(matrix)
+    for matrix in test_cases:
+        new_matrix = square_matrix_simple(matrix)
+        print(new_matrix)
 
