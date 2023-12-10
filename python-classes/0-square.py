bd74a4ec2f56ba44f025bc1b68c064618e5acb29
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-10 03:30:39 -0800

    this is the task's commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,18 @@
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            size (int): The size of the square.
+        """
         self.__size = size
 
 # Test the Square class
