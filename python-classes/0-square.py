c99ed2ba32db7ec10150cc44d79045c6ffe4b35b
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-10 03:26:27 -0800

    this is the task's commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -10,24 +10,24 @@ class Square:
 
 # Test the Square class
 if __name__ == "__main__":
-    my_square = Square(3)
+    mysquare = Square(3)
 
-    print(type(my_square))
-    print(my_square.__dict__)
+    print(type(mysquare))
+    print(mysquare.__dict__)
 
     try:
-        print(my_square.size)  # This should raise an AttributeError
+        print(mysquare.size)  # This should raise an AttributeError
     except AttributeError as e:
         print(e)
 
     try:
-        print(my_square.__size)  # This should raise an AttributeError
+        print(mysquare.__size)  # This should raise an AttributeError
     except AttributeError as e:
         print(e)
 
     # Accessing and modifying size using getter and setter
-    print(my_square.get_size())
+    print(mysquare.get_size())
 
-    my_square.set_size(5)
-    print(my_square.get_size())
+    mysquare.set_size(5)
+    print(mysquare.get_size())
 
