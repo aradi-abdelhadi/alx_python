abe324bb6855b7456d5be6466bfadd415e0df473
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-10 03:50:00 -0800

    this is the task's commit

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -66,5 +66,36 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for
+            for _ in range(self.__size):
+                print("#" * self.__size)
+
+    def __validate_size(self):
+        """
+        Validates the size attribute.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+# Test the Square class
+if __name__ == "__main__":
+    my_square = Square(3)
+    my_square.my_print()
+
+    print("--")
+
+    my_square.size = 10
+    my_square.my_print()
+
+    print("--")
+
+    my_square.size = 0
+    my_square.my_print()
+
+    print("--")
 
