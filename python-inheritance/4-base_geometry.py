90390fe7cf2d4e16d53951a9c5c11bb92259dd4a
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-18 09:15:37 -0800

    this is the tasks commit

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -5,20 +5,20 @@ class BaseGeometry:
     This module defines a class called BaseGeometry.
 
     The BaseGeometry class serves as a foundation for geometry-related classes.
-    It includes a public instance method area() that raises an Exception with the message "area() is not implemented".
+    It includes a public instance method area() that raises an exception with the message "area() is not implemented".
 
     Attributes:
     - No attributes are defined in this class.
 
     Methods:
-    - area(self): Raises an Exception with the message "area() is not implemented".
+    - area(self): Raises an exception with the message "area() is not implemented".
     """
     def area(self):
         """
         Placeholder method for calculating the area.
 
         Raises:
-        - Exception: Always raises an Exception with the message "area() is not implemented".
+        - NotImplementedError: Always raises NotImplementedError with the message "area() is not implemented".
         """
         raise NotImplementedError("area() is not implemented")
 
