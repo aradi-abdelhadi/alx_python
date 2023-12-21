cda57607e9f6b7471bb6bca78d8e13562358af1b
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-21 09:08:41 -0800

    this is the task's commit

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,8 +8,12 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         url = sys.argv[1]
         email = sys.argv[2]
-        payload = {'email': email}
-        response = requests.post(url, data=payload)
+        
+        # Ensure the correct URL formatting by adding '?' if needed
+        separator = '&' if '?' in url else '?'
+        url = f'{url}{separator}email={email}'
+        
+        response = requests.post(url)
         print("Your email is:", email)
         print(response.text)
     else:
