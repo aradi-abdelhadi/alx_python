151ce642f10cec6d6765599e4f3cd4e23bf927f7
aradi abdelhadi <ard.abdelhadi@gmail.com>
2023-12-21 09:19:57 -0800

    this is the task's commit

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,22 +1,10 @@
-#!/usr/bin/python3
-""" Script that takes in a URL and an email address, sends a POST request, and displays the response """
-
 import requests
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        url = sys.argv[1]
-        email = sys.argv[2]
-        
-        # Ensure the correct URL formatting by adding '?' if needed
-        separator = '&' if '?' in url else '?'
-        url = f'{url}{separator}email={email}'
-        
-        response = requests.post(url)
-        print("Your email is:", email)
-        print(response.text)
-    else:
-        print("Usage: ./2-post_email.py <URL> <email>")
-        sys.exit(1)
+url = sys.argv[1]  # Get the URL from the first command-line argument
+email = sys.argv[2]  # Get the email from the second command-line argument
+
+response = requests.post(url, data={"email": email})
+
+print(response.text)  # Print the body of the response
 
