97d6c538e172fe1281e24abe9ad5f66195377827
aradi abdelhadi <ard.abdelhadi@gmail.com>
2024-01-25 07:19:27 -0800

    Initial commit

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -1,20 +1,36 @@
 from flask import Flask, render_template
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    return "Hello HBNB!"
+    """
+    Route for the root endpoint.
 
+    Returns:
+        str: Greeting message.
+    """
+    return "Hello HBNB!"
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
+    """
+    Route to determine if a number is odd or even.
+
+    Args:
+        n (int): The input number.
+
+    Returns:
+        str: HTML template rendering the number and its parity.
+    """
+    # Check if the number is even or odd
     if n % 2 == 0:
         parity = "even"
     else:
         parity = "odd"
+    
+    # Render the template with the number and its parity
     return render_template('6-number_odd_or_even.html', number=n, parity=parity)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
