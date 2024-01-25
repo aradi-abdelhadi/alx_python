e889c664ff387f9e92b3891ab52a351b618fc53f
aradi abdelhadi <ard.abdelhadi@gmail.com>
2024-01-25 06:26:01 -0800

    commit

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -21,9 +21,9 @@ def hbnb():
 # Replace underscore _ symbols with a space
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+    return 'C {}'.format(escape(text).replace('_', ' '))
 
 if __name__ == '__main__':
     # Run the Flask application on 0.0.0.0:5000
     app.run(host='0.0.0.0', port=5000)
-:
+
