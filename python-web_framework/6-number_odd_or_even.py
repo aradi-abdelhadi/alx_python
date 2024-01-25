ef9a028db10380bd7b9d2dbddcb3fa7639f9660b
aradi abdelhadi <ard.abdelhadi@gmail.com>
2024-01-25 07:17:24 -0800

    Initial commit

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -7,32 +7,6 @@ def hello():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    return "C {}".format(text.replace("_", " "))
-
-
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-def python_route(text="is cool"):
-    return "Python {}".format(text.replace("_", " "))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    return "{} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('6-number_odd_or_even.html', number=n)
-
-
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     if n % 2 == 0:
